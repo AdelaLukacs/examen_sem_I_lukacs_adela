be76614aa32aba65ea10f491e8ac576d208ee479
AdelaLukacs <adela.lukacs03@e-uvt.ro>
2025-02-07 15:41:38 +0200

    update

---
+++ 7_delete.sql
@@ -0,0 +1,11 @@
+--constructii
+
+DELETE FROM constructii WHERE numar_parcela = 123;
+
+DELETE FROM constructii WHERE numar_parcela = 78; 
+
+ALTER TABLE constructii DROP COLUMN id_parcele;
+
+
+--parcele
+
