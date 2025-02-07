3399fbf1cb1df4de51f0f3f821b3cb8c231b4c86
AdelaLukacs <adela.lukacs03@e-uvt.ro>
2025-02-07 05:27:56 +0200

    update

--- 1_create.sql
+++ 1_create.sql
@@ -15,15 +15,6 @@ CREATE TABLE IF NOT EXISTS uat
 	id_judet INTEGER NOT NULL REFERENCES judet(fid_judet)
 );
 
-CREATE TABLE IF NOT EXISTS localitati
-(
-	fid_localitati SERIAL PRIMARY KEY,
-	nume_localitate VARCHAR (200) NOT NULL,
-	cod_siruta_localitati INTEGER UNIQUE NOT NULL,
-	tip_localitate VARCHAR (10) NOT NULL,
-	id_uat INTEGER NOT NULL REFERENCES uat(fid_uat)
-);
-
 CREATE TABLE IF NOT EXISTS sector_cadastral
 (
 	fid_sector_cadastral SERIAL PRIMARY KEY,
@@ -53,8 +44,8 @@ CREATE TABLE IF NOT EXISTS documente_cadastrale
 	id_documente_cadastrale SERIAL PRIMARY KEY,
 	tip_document VARCHAR (100) NOT NULL,
 	numar_document VARCHAR (50) NOT NULL,
-	emitent VARCHAR (100) NOT NULL,
-	data_emitere DATE NOT NULL,
+	emitent VARCHAR (100) NULL,
+	data_emitere DATE NULL,
 	id_imobile integer NOT NULL REFERENCES imobile(fid_imobile)
 );
 
@@ -70,8 +61,8 @@ CREATE TABLE IF NOT EXISTS proprietari
 	id_proprietari SERIAL PRIMARY KEY,
 	nume_proprietar VARCHAR (200) NOT NULL,
 	cnp_proprietar CHAR(13) UNIQUE NOT NULL,
-	adresa_proprietar VARCHAR (200) NOT NULL,
-	tip_proprietar VARCHAR (100) NOT NULL
+	adresa_proprietar VARCHAR (200) NULL,
+	tip_proprietar VARCHAR (100) NULL
 );
 
 CREATE TABLE IF NOT EXISTS proprietari_imobile
@@ -92,19 +83,11 @@ CREATE TABLE IF NOT EXISTS parcele
 	id_proprietari INTEGER NOT NULL REFERENCES proprietari (id_proprietari)
 );
 
-CREATE TABLE IF NOT EXISTS tip_constructii
-(
-	id_tip_constructii SERIAL PRIMARY KEY,
-	denumire_tip_constructii VARCHAR (200) NOT NULL,
-	descriere VARCHAR (200) NOT NULL
-);
-
 CREATE TABLE IF NOT EXISTS constructii
 (
 	id_constructii SERIAL PRIMARY KEY,
 	numar_parcela INTEGER NOT NULL,
 	suprafata DECIMAL NOT NULL,
-	adresa VARCHAR (200) NOT NULL,
-	id_tip_constructii INTEGER NOT NULL REFERENCES tip_constructii(id_tip_constructii),
+	adresa VARCHAR (200) NULL,
 	id_parcele INTEGER NOT NULL REFERENCES parcele(fid_parcele)
 );
---
+++ 2_insert.sql
@@ -0,0 +1,106 @@
+INSERT INTO judet (nume_judet, cod_siruta_judet) VALUES ('Arad', '29');
+
+INSERT INTO judet (nume_judet, cod_siruta_judet) VALUES ('Bihor', '56');
+
+INSERT INTO judet (nume_judet, cod_siruta_judet) VALUES ('Brasov', '83');
+
+INSERT INTO judet (nume_judet, cod_siruta_judet) VALUES ('Cluj', '127');
+
+INSERT INTO judet (nume_judet, cod_siruta_judet) VALUES ('Constanta', '136');
+
+INSERT INTO judet (nume_judet, cod_siruta_judet) VALUES ('Harghita', '190');
+
+INSERT INTO judet (nume_judet, cod_siruta_judet) VALUES ('Hunedoara', '207');
+
+INSERT INTO judet (nume_judet, cod_siruta_judet) VALUES ('Iasi', '225');
+
+INSERT INTO judet (nume_judet, cod_siruta_judet) VALUES ('Sibiu', '323');
+
+INSERT INTO judet (nume_judet, cod_siruta_judet) VALUES ('Timis', '350');
+
+
+INSERT INTO uat (nume_uat, cod_siruta_uat, tip_uat, id_judet) VALUES ('Arad', '9262', 'municipiu', '1');
+
+INSERT INTO uat (nume_uat, cod_siruta_uat, tip_uat, id_judet) VALUES ('Baile Felix', '32143', 'localitate', '2');
+
+INSERT INTO uat (nume_uat, cod_siruta_uat, tip_uat, id_judet) VALUES ('Rasnov', '40367', 'oras', '3');
+
+INSERT INTO uat (nume_uat, cod_siruta_uat, tip_uat, id_judet) VALUES ('Turda', '55259', 'municipiu', '4');
+
+INSERT INTO uat (nume_uat, cod_siruta_uat, tip_uat, id_judet) VALUES ('Constanta', '60419', 'municipiu', '5');
+
+INSERT INTO uat (nume_uat, cod_siruta_uat, tip_uat, id_judet) VALUES ('Praid', '85341', 'comuna', '6');
+
+INSERT INTO uat (nume_uat, cod_siruta_uat, tip_uat, id_judet) VALUES ('Deva', '86687', 'municipiu', '7');
+
+INSERT INTO uat (nume_uat, cod_siruta_uat, tip_uat, id_judet) VALUES ('Iasi', '95060', 'municipiu', '8');
+
+INSERT INTO uat (nume_uat, cod_siruta_uat, tip_uat, id_judet) VALUES ('Avrig', '144054', 'oras', '9');
+
+INSERT INTO uat (nume_uat, cod_siruta_uat, tip_uat, id_judet) VALUES ('Lugoj', '155350', 'municipiu', '10');
+
+
+INSERT INTO sector_cadastral (numar_sector, suprafata, id_uat) VALUES ('15', '256492', '1');
+
+INSERT INTO sector_cadastral (numar_sector, suprafata, id_uat) VALUES ('21', '249951', '2');
+
+INSERT INTO sector_cadastral (numar_sector, suprafata, id_uat) VALUES ('34', '32654', '3');
+
+INSERT INTO sector_cadastral (numar_sector, suprafata, id_uat) VALUES ('6', '254962', '4');
+
+INSERT INTO sector_cadastral (numar_sector, suprafata, id_uat) VALUES ('28', '254864', '5');
+
+INSERT INTO sector_cadastral (numar_sector, suprafata, id_uat) VALUES ('19', '265985', '6');
+
+INSERT INTO sector_cadastral (numar_sector, suprafata, id_uat) VALUES ('11', '235488', '7');
+
+INSERT INTO sector_cadastral (numar_sector, suprafata, id_uat) VALUES ('9', '125939', '8');
+
+INSERT INTO sector_cadastral (numar_sector, suprafata, id_uat) VALUES ('31', '165995', '9');
+
+INSERT INTO sector_cadastral (numar_sector, suprafata, id_uat) VALUES ('8', '189513', '10');
+
+
+INSERT INTO imobile (numar_imobil, suprafata, id_sector_cadastral) VALUES ('1234', '256', '1');
+
+INSERT INTO imobile (numar_imobil, suprafata, id_sector_cadastral) VALUES ('5678', '325', '2');
+
+INSERT INTO imobile (numar_imobil, suprafata, id_sector_cadastral) VALUES ('1357', '459', '3');
+
+INSERT INTO imobile (numar_imobil, suprafata, id_sector_cadastral) VALUES ('2468', '365', '4');
+
+INSERT INTO imobile (numar_imobil, suprafata, id_sector_cadastral) VALUES ('3693', '789', '5');
+
+INSERT INTO imobile (numar_imobil, suprafata, id_sector_cadastral) VALUES ('4848', '985', '6');
+
+INSERT INTO imobile (numar_imobil, suprafata, id_sector_cadastral) VALUES ('1551', '250', '7');
+
+INSERT INTO imobile (numar_imobil, suprafata, id_sector_cadastral) VALUES ('2332', '248', '8');
+
+INSERT INTO imobile (numar_imobil, suprafata, id_sector_cadastral) VALUES ('1425', '364', '9');
+
+INSERT INTO imobile (numar_imobil, suprafata, id_sector_cadastral) VALUES ('1352', '795', '10');
+
+
+INSERT INTO coordonate_imobile (latitudine, longitudine, id_imobile) VALUES ('46.1866', '21.3123', '1');
+
+INSERT INTO coordonate_imobile (latitudine, longitudine, id_imobile) VALUES ('46.9911', '21.9877', '2');
+
+INSERT INTO coordonate_imobile (latitudine, longitudine, id_imobile) VALUES ('45.5936', '25.4605', '3');
+
+INSERT INTO coordonate_imobile (latitudine, longitudine, id_imobile) VALUES ('46.5663', '23.7840', '4');
+
+INSERT INTO coordonate_imobile (latitudine, longitudine, id_imobile) VALUES ('44.1598', '28.6348', '5');
+
+INSERT INTO coordonate_imobile (latitudine, longitudine, id_imobile) VALUES ('46.5537', '25.1335', '6');
+
+INSERT INTO coordonate_imobile (latitudine, longitudine, id_imobile) VALUES ('45.8730', '22.9110', '7');
+
+INSERT INTO coordonate_imobile (latitudine, longitudine, id_imobile) VALUES ('47.1585', '27.6014', '8');
+
+INSERT INTO coordonate_imobile (latitudine, longitudine, id_imobile) VALUES ('45.7167', '24.3721', '9');
+
+INSERT INTO coordonate_imobile (latitudine, longitudine, id_imobile) VALUES ('45.6886', '21.9035', '10');
+
+
+INSERT INTO documente_cadastrale (tip_document, numar_document, data_emitere, id_imobile) VALUES ('', '', '', '');
